9833848881f760611640e1f034838ce7af56e0da
Sultan Karimov <skarimovkgz@gmail.com>
2019-12-19 14:32:49 -0600

    finalizing

--- script/script.js
+++ script/script.js
@@ -1,20 +1,26 @@
-saySomthn = () => {
-    if (Window.hasOwnProperty('webkitSpeechRecognition')) {
-        var recognition = new webkitSpeechRecognition();
-        recognition.continuous = false;
-        recognition.interimResults = false;
 
-        recognition.lang = "en-US";
-        recognition.start();
+  function saySomething() {
 
-        recognition.onresult = function (e) {
-            document.getElementById('trnascript').value = e.results[0][0].transcript;
-            recognition.stop();
-            document.getElementById('speak').submit();
-        };
-        recognition.onerror = function (e) {
-            recognition.stop();
-        }
-        }
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+      var recognition = new webkitSpeechRecognition();
+
+      recognition.continuous = false;
+      recognition.interimResults = false;
+
+      recognition.lang = "en-US";
+      recognition.start();
+
+      recognition.onresult = function(e) {
+        document.getElementById('transcript').value
+                                 = e.results[0][0].transcript;
+        recognition.stop();
+        document.getElementById('speak').submit();
+      };
+
+      recognition.onerror = function(e) {
+        recognition.stop();
+      }
+
     }
-}
+  }
